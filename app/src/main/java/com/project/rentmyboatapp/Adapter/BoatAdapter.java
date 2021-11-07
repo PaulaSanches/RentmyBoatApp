c05381780cadb0960d28a9f4b19509ae01b3509e
Paula Sanches <paula.correabarrientos@gmail.com>
2021-11-07 20:34:03 +1300

    Merged User Interface successfully.

--- app/src/main/java/com/project/rentmyboatapp/Adapter/BoatAdapter.java
+++ app/src/main/java/com/project/rentmyboatapp/Adapter/BoatAdapter.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.project.rentmyboatapp.Boat;
+
 import com.project.rentmyboatapp.BoatRenter;
 import com.project.rentmyboatapp.R;
 
