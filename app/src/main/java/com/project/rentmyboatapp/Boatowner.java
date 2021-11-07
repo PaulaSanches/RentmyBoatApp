b0615599b4bc3bb95d44a3437dfd568c48956336
Paula Sanches <paula.correabarrientos@gmail.com>
2021-11-07 17:58:47 +1300

    Save collections with boatpic sucessful to Firestore.

--- app/src/main/java/com/project/rentmyboatapp/Boatowner.java
+++ app/src/main/java/com/project/rentmyboatapp/Boatowner.java
@@ -4,43 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 import com.project.rentmyboatapp.user.User;
 
-import java.io.File;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
-import java.util.jar.Attributes;
-
-import settings.FirebaseSettings;
 
 public class Boatowner extends AppCompatActivity {
 
